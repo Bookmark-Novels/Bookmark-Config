f4e90ed8d62d349398b12bd1b1332b1b6b3ef98e
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-11 23:09:38 -0700

    add caching

--- tests/config_test.py
+++ tests/config_test.py
@@ -7,6 +7,4 @@ from bookmark_config import Config
 
 def test_connect_gatekeeper():
     test_config = Config('gatekeeper.consul.dev.bookmark.services')
-    test_config.load()
-
     assert test_config.getBoolean('gatekeeper', 'config', 'debug') is True
